baaec2b12a8c2d7d175d3f2ba2b67cce792409fd
Deborah Digges <deborah.gertrude.digges@gmail.com>
2016-02-06 11:41:27 +0530

    Add separate JS for map

---
+++ map.js
@@ -0,0 +1,29 @@
+var w = 600;
+var h = 600;
+var proj = d3.geo.mercator();
+var path = d3.geo.path().projection(proj);
+var t = proj.translate(); // the projection's default translation
+var s = proj.scale() // the projection's default scale
+var map = d3.select("#chart").append("svg:svg")
+                            .attr("width", w)
+                            .attr("height", h)
+                            .call(initialize);
+
+var india = map.append("svg:g")
+.attr("id", "india");
+
+d3.json("data/states.json", function (json) {
+    india.selectAll("path")
+    .data(json.features)
+    .enter().append("path")
+    .attr("d", path)
+    .append("svg:title")
+    .text(function(d) { return d.id; });
+
+});
+
+
+function initialize() {
+    proj.scale(6700);
+    proj.translate([-1240, 720]);
+}
